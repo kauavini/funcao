330d9b35853cc73eeda2392abf0094000f0ee564
kauavini <45333104+kauavini@users.noreply.github.com>
2018-12-17 23:48:59 -0200

    Create Questao3.py

---
+++ Questao3.py
@@ -0,0 +1,7 @@
+def somaTresValores(a, b, c):
+  s = a + b + c
+  print(s)
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite um valor: '))
+n3 = int(input('Digite um valor: '))
+somaTresValores(n1, n2, n3)
